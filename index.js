52cbfda43b44613955664ad80c6805a861e6be5f
“[fmhsakibbgit config --global user.email “[vmhssaakkiibb@gmail.com <mhssaakkiibb@gmail.com>
2026-02-27 21:58:24 +0600

    Initial commit: project structure and basic program

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Name: Mosharef Hossain");
+console.log("Today's date:", new Date().toLocaleDateString());
---
+++ main.py
@@ -0,0 +1,4 @@
+from datetime import date
+
+print("Name: Mosharef Hossain")
+print("Today's date:", date.today())
