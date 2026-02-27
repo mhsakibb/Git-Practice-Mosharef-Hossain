b4f5c0fb1631eea65aa577553f9ca01639913345
“[fmhsakibbgit config --global user.email “[vmhssaakkiibb@gmail.com <mhssaakkiibb@gmail.com>
2026-02-27 22:24:54 +0600

    Add basic calculator functions

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const { add, subtract } = require('./utils');
+
 console.log("Name: Mosharef Hossain");
 console.log("Today's date:", new Date().toLocaleDateString());
+
+// Test calculator functions
+console.log("5 + 3 =", add(5, 3));
+console.log("10 - 4 =", subtract(10, 4));
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from utils import add, subtract
 from datetime import date
 
 print("Name: Mosharef Hossain")
 print("Today's date:", date.today())
+
+# Test calculator functions
+print("5 + 3 =", add(5, 3))
+print("10 - 4 =", subtract(10, 4))
