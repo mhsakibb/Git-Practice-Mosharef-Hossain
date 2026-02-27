bf33fd14b2907e94218135728afb6402044cad7a
“[fmhsakibbgit config --global user.email “[vmhssaakkiibb@gmail.com <mhssaakkiibb@gmail.com>
2026-02-27 22:39:21 +0600

    Add multiply function to calculator

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { add, subtract } = require('./utils');
+const { add, subtract, multiply} = require('./utils');
 
 console.log("Name: Mosharef Hossain");
 console.log("Today's date:", new Date().toLocaleDateString());
@@ -6,3 +6,4 @@ console.log("Today's date:", new Date().toLocaleDateString());
 // Test calculator functions
 console.log("5 + 3 =", add(5, 3));
 console.log("10 - 4 =", subtract(10, 4));
+console.log("5 * 3 =", multiply(5, 3));
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import add, subtract
+from utils import add, subtract, multiply
 from datetime import date
 
 print("Name: Mosharef Hossain")
@@ -7,3 +7,4 @@ print("Today's date:", date.today())
 # Test calculator functions
 print("5 + 3 =", add(5, 3))
 print("10 - 4 =", subtract(10, 4))
+print("5 * 3 =", multiply(5, 3))
